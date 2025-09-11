486c60dc663d74ac16b134ad1b22fa9b6688babe
Sila Ayas <115110100+simmilay@users.noreply.github.com>
2025-09-11 16:50:48 +0300

    feture:the user interface has been made dynamic

--- app.js
+++ app.js
@@ -1,38 +1,34 @@
-/*const choice = document.getElementById("choice")
-const lengths =document.getElementsById("lengths")
-const weights =document.getElementsById("weights")
-const temperatures =document.getElementsById("temperatures")
-const volumes =document.getElementsById("volumes")
-*/
+const units = {
+  lengths: ["metre", "cantimetre", "feet", "inc"],
+  weights: ["kilogram", "gram", "pound"],
+  temperatures: ["celcius", "fahrenheit", "kelvin"],
+  volumes: ["Liter", "mililiter", "galon"],
+};
 
-/* choice.addEventListener("change" , function (event)
-{
-    if(event.target.value == 'lengths' ){
-        lengths.style.display == "block" 
-        weights.style.display == "none"
-        temperatures.style.display =="none"
-        volumes.style.display == "none"
-    }else if(event.target.value == 'weights'){
-        lengths.style.display == "none" 
-        weights.style.display == "block"
-        temperatures.style.display =="none"
-        volumes.style.display == "none"
-    }else if(event.target.value == 'temperatures'){
-        lengths.style.display == "none" 
-        weights.style.display == "none"
-        temperatures.style.display =="block"
-        volumes.style.display == "none"
-    }else if(event.target.value == 'weights'){
-        lengths.style.display == "none" 
-        weights.style.display == "none"
-        temperatures.style.display =="none"
-        volumes.style.display == "block"
-    }
+function getToConverter(selectedElement) {
+  const first_converter = document.getElementById("firstConverter");
+  const second_converter = document.getElementById("secondConverter");
 
-});
- */
+  first_converter.innerHTML = "";
+  second_converter.innerHTML = "";
 
-function setSelectionValue(newValue) {
+  const selectedValues = units[selectedElement.value];
+
+  selectedValues.forEach((element) => {
+    const option1 = document.createElement("option");
+    option1.value = element;
+    option1.innerText = element;
+
+    const option2 = document.createElement("option");
+    option2.value = element;
+    option2.innerText = element;
+
+    firstConverter.appendChild(option1);
+    secondConverter.appendChild(option2);
+  });
+}
+
+/*function setSelectionValue(newValue) {
   const dropdown = document.getElementById("choice");
   const value = dropdown.value;
   if (value == "volumes") {
@@ -68,8 +64,7 @@ function convert_to_length() {
     .getElementById("length_2_text")
     .value.toString();
 
-
-    //conversions with meters
+  //conversions with meters
   if (value_length_1 == "metre") {
     if (value_length_2 == "cantimetre") {
       //işlem formül yazılacak
@@ -93,7 +88,7 @@ function convert_to_length() {
       // document.getElementById("length_text_out") = document.getElementById("length_text_in");
       document.getElementById("length_2_text").value = length_text_in;
     }
-  } 
+  }
   //conversions with cantimeters
   else if (value_length_1 == "cantimetre") {
     if (value_length_2 == "metre") {
@@ -117,20 +112,19 @@ function convert_to_length() {
     } else {
       //cantimetre yazılacak
       document.getElementById("length_2_text").value = length_text_in;
-
     }
-  } 
+  }
   //conversions with feet
   else if (value_length_1 == "feet") {
     if (value_length_2 == "metre") {
       //işlem yazılacak
- var converter_result = parseFloat(length_text_in) / 3.2808;
+      var converter_result = parseFloat(length_text_in) / 3.2808;
       if (!isNaN(converter_result)) {
         document.getElementById("length_2_text").value = converter_result;
       }
     } else if (value_length_2 == "cantimetre") {
       //işlem yazılacak
-       var converter_result = parseFloat(length_text_in) * 30.48;
+      var converter_result = parseFloat(length_text_in) * 30.48;
       if (!isNaN(converter_result)) {
         document.getElementById("length_2_text").value = converter_result;
       }
@@ -168,7 +162,68 @@ function convert_to_length() {
     } else {
       //inc yazılacak
       document.getElementById("length_2_text").value = length_text_in;
-
     }
   }
 }
+
+function convert_to_weight() {
+  const dropdown_weight_1 = document.getElementById("weight_1_select");
+  const dropdown_weight_2 = document.getElementById("weight_2_select");
+  const value_weight_1 = dropdown_weight_1.value;
+  const value_weight_2 = dropdown_weight_2.value;
+  var weight_text_in = document.getElementById("weight_1_text").value;
+  var weight_text_out = document
+    .getElementById("weight_2_text")
+    .value.toString();
+//conversions with kilogram
+  if (value_weight_1 == "kilogram") {
+    if (value_weight_2 == "gram") {
+      //işlem yazılacak
+      var converter_result_w = parseFloat(weight_text_in) * 1000;
+      if (!isNaN(converter_result_w)) {
+        document.getElementById("weight_2_text").value = converter_result_w;
+      }
+    } else if (value_weight_2 == "pound") {
+      //işlem yazılacak
+      var converter_result_w = parseFloat(weight_text_in) * 2.2046226;
+      if (!isNaN(converter_result_w)) {
+        document.getElementById("weight_2_text").value = converter_result_w;
+      }
+    } else {
+      document.getElementById("weight_2_text").value = weight_text_in;
+    }
+  }
+//conversions with gram
+  else if (value_weight_1 == "gram") {
+    if (value_weight_2 == "kilogram") {
+      var converter_result_w = parseFloat(weight_text_in) / 1000;
+      if (!isNaN(converter_result_w)) {
+        document.getElementById("weight_2_text").value = converter_result_w;
+      }
+    } else if (value_weight_2 == "kilogram") {
+      var converter_result_w = parseFloat(weight_text_in) * 0.0220462262;
+      if (!isNaN(converter_result_w)) {
+        document.getElementById("weight_2_text").value = converter_result_w;
+      }
+    } else {
+      document.getElementById("weight_2_text").value = weight_text_in;
+    }
+  } else if (value_weight_1 == "pound") {
+    //işlem
+    if(value_weight_2 == "kilogram"){
+         var converter_result_w = parseFloat(weight_text_in) * 0.45359237;
+      if (!isNaN(converter_result_w)) {
+        document.getElementById("weight_2_text").value = converter_result_w;
+      }
+    }else if(value_weight_2 == "gram"){
+         var converter_result_w = parseFloat(weight_text_in) * 453.59237;
+      if (!isNaN(converter_result_w)) {
+        document.getElementById("weight_2_text").value = converter_result_w;
+      }
+    }else{
+      document.getElementById("weight_2_text").value = weight_text_in;
+
+    }
+  }
+]
+*/
